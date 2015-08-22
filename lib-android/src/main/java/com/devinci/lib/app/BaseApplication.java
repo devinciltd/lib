a27f01533ad0574896127eaca2eecdff032156b3
Tomasz Rozbicki <tomrozb+code@gmail.com>
2015-08-22 11:39:22 +0200

    Add lib-android and sample modules

---
+++ lib-android/src/main/java/com/devinci/lib/app/BaseApplication.java
@@ -0,0 +1,10 @@
+package com.devinci.lib.app;
+
+import android.app.Application;
+
+/**
+ * Base application with global state.
+ */
+public class BaseApplication extends Application {
+
+}
---
+++ sample/src/main/java/com/devinci/lib/sample/app/App.java
@@ -0,0 +1,7 @@
+package com.devinci.lib.sample.app;
+
+import com.devinci.lib.app.BaseApplication;
+
+public class App extends BaseApplication {
+
+}
