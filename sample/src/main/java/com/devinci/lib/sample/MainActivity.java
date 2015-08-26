85d3fdf253566cc797099077bb6b79a105596161
Tomasz Rozbicki <tomrozb+code@gmail.com>
2015-08-26 23:59:13 +0200

    Add toolbar layout

--- sample/src/main/java/com/devinci/lib/sample/MainActivity.java
+++ sample/src/main/java/com/devinci/lib/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.devinci.lib.sample;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import com.devinci.lib.activity.BaseActivity;
 
 public class MainActivity extends BaseActivity {
@@ -8,6 +9,9 @@ public class MainActivity extends BaseActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.lib_toolbar);
+    setSupportActionBar(toolbar);
 
     // Crash reporting should be injected (crashReporting != null)
     crashReporting.sendException(new RuntimeException());
