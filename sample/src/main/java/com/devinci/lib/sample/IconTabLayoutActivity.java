5713c4347bd6f7a6f5899850190ff85c317e5e6c
Artur Stepniewski <artur@dicedmelon.com>
2015-08-29 21:33:34 +0200

    Added helper class for Toolbar title setting
    ToolbarUtils contains method that registers
    Toolbar to receive notifications from ViewPager
    when a page changes so it can use the page title
    as its own title.

--- sample/src/main/java/com/devinci/lib/sample/IconTabLayoutActivity.java
+++ sample/src/main/java/com/devinci/lib/sample/IconTabLayoutActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import static com.devinci.lib.view.ToolbarUtils.setupPageSelectionNotifications;
 
 public class IconTabLayoutActivity extends AppCompatActivity {
 
@@ -11,8 +14,11 @@ public class IconTabLayoutActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_icontablayout);
 
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     TabLayout tabLayout = (TabLayout) findViewById(R.id.icon_tab_layout);
     ViewPager viewPager = (ViewPager) findViewById(R.id.layout_view_pager);
     tabLayout.setupWithViewPager(viewPager);
+
+    setupPageSelectionNotifications(toolbar, viewPager);
   }
 }
